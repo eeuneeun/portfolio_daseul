1be33a003974dbacd8b4806c738bff44db6f594f
Kim Daseul <osl123o@naver.com>
2025-04-19 14:58:49 +0900

    설정 완

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'ES6'  // ES6에 해당
+  },
+  esbuild: {
+    target: 'ES6'
+  }
 })
+
+// const { defineConfig } = require('vite');
+// // 혹은 import * as react from '@vitejs/plugin-react';
+// const react = require('@vitejs/plugin-react');
+
+// module.exports = defineConfig({
+//   plugins: [react()],
+// });
